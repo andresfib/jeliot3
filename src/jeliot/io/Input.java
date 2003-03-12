82a3a1b38d83f38d8e209908e49e26969898a0cb
amoreno <andresfib@users.noreply.github.com>
2003-03-12 16:39:07 +0000

    Added basic input and output classes (jeliot.io) and some support to these in EvaluationVisitor.java -AM

---
+++ src/jeliot/io/Input.java
@@ -0,0 +1,25 @@
+package jeliot.io;
+
+/**
+ * Nearly empty class.
+ * Unknown purpose.
+ * Maybe for debugging or a superclass.
+ * 
+ * @author Pekka Uronen
+ */
+public class Input {
+
+	/**
+	 * Method that always returns int-type value 0 .
+	 */
+    public static int readInt() {
+        return 0;
+	}
+
+	/**
+	 * Method that always returns double-type value 0.0 .
+	 */
+    public static double readDouble() {
+        return 0.0;
+	}
+}
