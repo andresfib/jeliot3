6e7b308739f93f7b959b726cb3127a92d99d8a42
nmyller <nmyller@e970d705-5e49-0410-aae4-19a007768b50>
2003-05-14 00:08:10 +0000

    First versions of the files that represents the reference semantics and arrays. -NM

---
+++ trunk/src/jeliot/lang/ArrayUtilities.java
@@ -0,0 +1,59 @@
+package jeliot.lang;
+
+import java.lang.reflect.*;
+import java.util.*;
+import jeliot.FeatureNotImplementedException;
+import jeliot.theatre.*;
+
+public class ArrayUtilities {
+
+    public static boolean nextIndex(int[] indexCounters, int[] lengths) {
+        int length = lengths.length;
+        int index = length - 1;
+
+        while (index >= 0) {
+
+            indexCounters[index]++;
+
+            if (indexCounters[index] < lengths[index]) {
+                break;
+            }
+
+            indexCounters[index] = 0;
+            index--;
+        }
+
+        for (int i = 0; i < length - 1; i++) {
+            if (indexCounters[i] != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Object getObjectAt(Object array, int[] index) {
+        Object tempArray = array;
+        int n = index.length;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                return Array.get(tempArray, index[i]);
+            } else {
+                tempArray = Array.get(tempArray, index[i]);
+            }
+        }
+        return null;
+    }
+
+    public static void setObjectAt(Object array, int[] index, Object newObject) {
+        Object tempArray = array;
+        int n = index.length;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                Array.set(array, index[i], newObject);
+            } else {
+                tempArray = Array.get(tempArray, index[i]);
+            }
+        }
+    }
+
+}
