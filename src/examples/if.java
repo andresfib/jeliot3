64b685ac9bcebd1e7fb8e1c2fd330f0ca5234522
amoreno <andresfib@users.noreply.github.com>
2003-06-24 11:05:56 +0000

    Examples added

---
+++ src/examples/if.java
@@ -0,0 +1,13 @@
+import jeliot.io.*;
+
+public class MyClass {
+    public static void main() {
+
+        // Your algorithm goes here.
+       int a=2, b=1;      
+    if (a==b)
+       { Output.println('x'); }
+    else
+       { Output.println('y'); }
+    }
+}
