9d742bf013eff4686273b5e6510d51fe165f7bdc
jeliot <jeliot@cs.uef.fi>
2004-09-09 13:54:44 +0000

    Replaced java command with javaw -AM

--- src/jeliot.bat
+++ src/jeliot.bat
@@ -1,10 +1,10 @@
 IF EXIST jre\bin\java.exe GOTO JeliotJRE
 
 :JeliotCommon
-java -jar jeliot.jar %1 %2 %3
+javaw -jar jeliot.jar %1 %2 %3
 GOTO END
 
 :JeliotJRE
-jre\bin\java -jar jeliot.jar %1 %2 %3
+jre\bin\javaw -jar jeliot.jar %1 %2 %3
 
 :END
