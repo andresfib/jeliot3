1a20543e2db97503828a5acecd0957d4cbd1ca77
jeliot <jeliot@cs.uef.fi>
2004-12-01 10:18:35 +0000

    Added new test Examples -AM

--- examples/If.java
+++ examples/If.java
@@ -1,13 +1,23 @@
-import jeliot.io.*;
-
-public class If {
-    public static void main() {
-
-        // Your algorithm goes here.
-       int a=2, b=1;
-    if (a==b)
-       { Output.println('x'); }
-    else
-       { Output.println('y'); }
-    }
+ class Ware {
+int i = 0;
+static int z = 0;
+Ware (int i) {
+this.i  = i;
+z++;
 }
+}    
+ 
+     public class MeinProgramm { 
+       public static void main (String [] args) {
+         int [] f = {1,2,3};
+Ware [] waren = new Ware [3];
+for (int j = 0; j < f.length; j++){
+waren[j] = new Ware (f[j]);
+}
+for (int j = 0; j < f.length; j++){
+System.out.println (waren [j].i);
+}
+ 
+
+       } // Ende main
+       }
