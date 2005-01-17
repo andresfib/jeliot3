4586f4c689f8646204cbd19b56456a71c5550f6d
jeliot <jeliot@e970d705-5e49-0410-aae4-19a007768b50>
2005-01-17 08:09:09 +0000

    *** empty log message ***

---
+++ trunk/testFiles/3.java
@@ -0,0 +1,39 @@
+//Class:Application
+
+import jeliot.io.*;
+
+public class Application {
+    
+	public static void main() {
+    	char[] array1 = new char[] {'g', 'c', 't', 'c', 'g', 'g', 'c', 'a'};
+
+    	char[] array2 = new char[] {'g', 'c', 'a'};
+    	
+    	if (task(array2, array1)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");            
+        }
+	}
+	
+	static boolean task(char[] array2, char[] array1) { 
+        int length1 = array1.length;
+        int length2 = array2.length;
+        int value = length2 - length1;
+        int index1;
+        int index2; 
+  
+        for (index1 = 0; index1 <= value; index1++) { 
+            for (index2 = 0; index2 < length2; index2++) {
+            	if (array2[index2] != array1[index1 + index2]) {
+            		break;
+            	}
+            }
+       
+            if (index2 >= length2) { 
+                return true;
+            } 
+        }
+        return false;
+    }
+}
--- trunk/tests/test/jeliot/JeliotTest.java
+++ trunk/tests/test/jeliot/JeliotTest.java
@@ -5,6 +5,10 @@ package test.jeliot;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,25 +22,115 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
+ * This test should be run against the files in the testFiles directory.
+ * So the context for these tests is the testFiles directory and it is assumed.
+ * 
+ * You can control the test with the INPUT_GIVEN variable that is set <code>true</code> when
+ * there is someone giving inputs to the programs when needed and <code>false</code> when
+ * the tests are run stand-alone without input.
+ * 
+ * When adding new files to the testFiles directory, there are few thing to remember.
+ * This test assumes that the file name is also the class name that contains the main method.
+ * If this is not the case then the class name should be indicated with <code>//Class:ClassNameHere</code>
+ * in the beginning of the file. Secondly, the method main is assumed to contain no parameters so it is
+ * of form <code>main()</code> if the method is something else it needs to be indicated with
+ * <code>//Call-Method:HereComesTheMethodCall</code> (e.g. <code>//Call-Method:main(new String[0])</code>
+ * or <code>//Call-Method:SomeMethod()</code>). Thirdly, if the program needs any input then it should be
+ * indicated with <code>//Needs_Input</code> code in the beginning of the file. Fourthly, if the method call
+ * is non-static for example a constructor call such as <code>//Call-Method:new ClockDisplay()</code> then
+ * it should be indicated with the code <code>//NonStaticCall</code>.
+ * 
  * @author nmyller
  */
 public class JeliotTest extends TestCase {
 
-    private static final String mainMethodCall = "main();";
-    private static final String mainMethodCallWithArgs = "main(new String[0]);";
-    
+    private static final boolean INPUT_GIVEN = false;
+
+    private static final String MAIN_METHOD_CALL = "main()";
+
+    //private static final String mainMethodCallWithArgs = "main(new String[0]);";
+
+    private static final String CLASS = "//Class:";
+
+    private static final String CALL_METHOD = "//Call-Method:";
+
+    private static final String NEEDS_INPUT = "//Needs_Input";
+
+    private static final String NON_STATIC_CALL = "//NonStaticCall";
+
     public void testJeliot() {
-        runJeliotWithClassAndMethod("If");
+        File file = new File(System.getProperties().getProperty("user.dir"));
+
+        File[] files = file.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return (name.indexOf(".java") >= 0);
+            }
+        });
+
+        int n = files.length;
+        for (int i = 0; i < n; i++) {
+            String fileName = files[i].getName();
+            String className = fileName.substring(0, fileName.length() - 5);
+            String calledMethod = MAIN_METHOD_CALL;
+            boolean needsInput = false;
+            boolean staticCall = true;
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(files[i]));
+                String str = null;
+
+                while ((str = br.readLine()) != null) {
+                    int index;
+                    if ((index = str.indexOf(CLASS)) >= 0) {
+                        className = str.substring(index + CLASS.length());
+                    }
+
+                    if ((index = str.indexOf(CALL_METHOD)) >= 0) {
+                        calledMethod = str.substring(index
+                                + CALL_METHOD.length());
+                    }
+
+                    if ((index = str.indexOf(NEEDS_INPUT)) >= 0) {
+                        needsInput = true;
+                    }
+
+                    if ((index = str.indexOf(NON_STATIC_CALL)) >= 0) {
+                        staticCall = false;
+                    }
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (INPUT_GIVEN || needsInput == INPUT_GIVEN) {
+                runJeliotWithClassAndMethod(fileName, className, calledMethod,
+                        staticCall);
+            }
+        }
+
     }
-    
-    public void runJeliotWithClassAndMethod(String className) {
-        Jeliot j = Jeliot.start(new String[] {"", "true", className + ".java"});
+
+    public void runJeliotWithClassAndMethod(String fileName, String className,
+            String calledMethod, boolean staticCall) {
+
+        System.out.println(fileName + ":" + className + ":" + calledMethod
+                + ":" + staticCall);
+
+        Jeliot j = Jeliot.start(new String[] { "", "true", fileName });
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        j.compile(className + "." + mainMethodCall);
+
+        if (staticCall) {
+            j.compile(className + "." + calledMethod + ";");
+        } else {
+            j.compile(calledMethod + ";");
+        }
+
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -50,7 +144,7 @@ public class JeliotTest extends TestCase {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }            
+            }
         }
         JSlider slider = jw.getSpeedSlider();
         slider.setValue(slider.getMaximum());
@@ -65,27 +159,31 @@ public class JeliotTest extends TestCase {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }                        
+            }
         }
-        assertTrue("Error Message is shown instead of Theater view.", jw.getTheaterPane() instanceof JTabbedPane);
+        assertTrue("Error Message is shown instead of Theater view in file "
+                + fileName + ".", jw.getTheaterPane() instanceof JTabbedPane);
         //edit.doClick();
         JFrame frame = jw.getFrame();
         WindowListener[] wl = frame.getWindowListeners();
         int n = wl.length;
         for (int i = 0; i < n; i++) {
-            wl[i].windowClosing(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+            wl[i].windowClosing(new WindowEvent(frame,
+                    WindowEvent.WINDOW_CLOSING));
         }
+        frame.dispose();
+        frame.setVisible(false);
         j = null;
         jw = null;
         frame = null;
         play = null;
         edit = null;
     }
-    
+
     public static Test suite() {
         return new TestSuite(JeliotTest.class);
     }
-    
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(suite());
         //System.exit(0);
