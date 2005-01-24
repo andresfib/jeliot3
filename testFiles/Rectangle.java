562dbe057ab9aea164627a3d793cf598a7a8acaa
jeliot <jeliot@cs.uef.fi>
2005-01-24 12:25:29 +0000

    *** empty log message ***

---
+++ testFiles/Rectangle.java
@@ -0,0 +1,73 @@
+//Class:Rectange
+//Call-Method:main(new String[0])
+
+import java.io.*;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    public boolean isAbove(Point p) {
+        if (y < p.y) {
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isBelow(Point p) {
+        if (y > p.y)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isLeftTo(Point p) {
+        if (x < p.x)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isRightTo(Point p) {
+        if (x > p.x)
+            return true;
+        else
+            return false;
+    }
+}
+
+
+public class Rectangle {
+    private Point top_left;
+    private Point bottom_right;
+
+    public Rectangle(Point tl, Point br) {
+        top_left = tl;
+        bottom_right = br;
+    }
+
+    public boolean cointains(Point p) {
+        if (top_left.isAbove(p) && top_left.isLeftTo(p) &&
+            bottom_right.isBelow(p) && bottom_right.isRightTo(p))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String args[]) {
+        Point myPoint = new Point(12,6);
+        Point topLeft = new Point(6,10);
+        Point bottomRight = new Point(16,4);
+        Rectangle myRectangle = new Rectangle(topLeft, bottomRight);
+
+        if (myRectangle.contains(myPoint))
+            System.out.println("The point is withing the rectangle");
+        else
+            System.out.println("The point is outside the rectangle");
+    }
+}
