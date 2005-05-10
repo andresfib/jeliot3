e296bf2276d24f2dcd2dddd05d51463f8a69c31d
jeliot <jeliot@cs.uef.fi>
2005-05-10 12:35:37 +0000

    Updated Reminders for experiment -AM

--- src/jeliot/tracker/Reminder.java
+++ src/jeliot/tracker/Reminder.java
@@ -1,8 +1,11 @@
-package jeliot.tracker;
+package jeliot.tracker;	
 
 import java.awt.Toolkit;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  * Simple demo that uses java.util.Timer to schedule a task 
@@ -12,23 +15,39 @@ import java.util.TimerTask;
 public class Reminder {
     Timer timer;
     Timer  timer2;
+    JFrame frame;
     
+    
+    /**
+     * Seconds until the end of the experiment.
+     * @param seconds
+     */
     public Reminder(int seconds) {
         timer = new Timer();
         timer2 = new Timer();
         
-        timer.schedule(new RemindTask(), seconds*1000);
-        timer.schedule(new ExitTask(), (120*1000)*(seconds*1000));
+        //A beep reminder will sound 2 minutes before the end of the experiment
+        timer.schedule(new RemindTask(), (seconds*1000)-(120*1000));
+        //End happens 
+        timer.schedule(new ExitTask(),(seconds*1000));
 }
 
     class RemindTask extends TimerTask {
         public void run() {
+        	Tracker.trackEvent(TrackerClock.currentTimeMillis(), Tracker.OTHER, -1, -1, "Sound");
         	Toolkit.getDefaultToolkit().beep();
         }
     }
     class ExitTask extends TimerTask{
     	public void run(){
-    		System.exit(1);
+    		Object[] options = { "OK" };
+    		Tracker.trackEvent(TrackerClock.currentTimeMillis(), Tracker.OTHER, -1, -1, "End of experiment");
+    		Toolkit.getDefaultToolkit().beep();
+    		Tracker.trackEvent(TrackerClock.currentTimeMillis(), Tracker.OTHER, -1, -1, "Sound");
+            int n = JOptionPane.showOptionDialog(frame, "Experiment is OVER, thanks for collaborating!",
+            		"Experiment Finished", JOptionPane.OK_OPTION, 
+					JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);            
+    		
     	}
     }
 
