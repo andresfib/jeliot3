e509e40560ee58be5951e3b0e58a353b540f9b50
jeliot <jeliot@cs.uef.fi>
2005-09-26 13:34:03 +0000

    *** empty log message ***

--- src/koala/dynamicjava/util/ParamTypes.java
+++ src/koala/dynamicjava/util/ParamTypes.java
@@ -71,8 +71,8 @@ public class ParamTypes {
 	/**
 	 * Compares two method's list of parameters
 	 * 
-	 * @param paramTypes1
-	 * @param paramTypes2
+	 * @param paramTypes1 Formal parameters
+	 * @param paramTypes2 Actual parameters
 	 * @return Returns true if both of them are the same, false otherewise
 	 */
 	public static boolean compareSignatures(Class[] paramTypes1,
@@ -91,11 +91,20 @@ public class ParamTypes {
 			for (int i = 0; i < n; i++) {
 				paramTypes1[i] = resolveType(paramTypes1[i]);
 				paramTypes2[i] = resolveType(paramTypes2[i]);
+                if (isPrimitive(paramTypes1[i]) && isPrimitive(paramTypes2[i])) {
+                    if (isTypeAssignableFromType(paramTypes1[i], paramTypes2[i]));
+                } else {
+                    if (!paramTypes1[i].isAssignableFrom(paramTypes2[i])) {
+                        return false;
+                    }
+                }
+                /* Too simplistic
 				String p1 = paramTypes1[i].getName();
 				String p2 = paramTypes2[i].getName();
 				if (!p1.equals(p2)) {
 					return false;
 				}
+                */
 				i++;
 			}
 
@@ -104,6 +113,67 @@ public class ParamTypes {
 		}
 		return true;
 	}
+    
+    private static boolean isTypeAssignableFromType(Class c1, Class c2) {
+        if (classEquals(Integer.class, c2)) {
+            //int to int, long, float, or double
+            if (classEquals(c1, Integer.class) || classEquals(c1, Long.class) || classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }
+        } else if (classEquals(Double.class, c2)) {
+            //double to double
+            if (classEquals(c1, Double.class)) {
+                return true;
+            }            
+        } else if (classEquals(Byte.class, c2)) {
+            //byte to byte, short, int, long, float, or double
+            if (classEquals(c1, Byte.class) || classEquals(c1, Short.class) ||  classEquals(c1, Integer.class) || classEquals(c1, Long.class) || classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }
+        } else if (classEquals(Long.class, c2)) {
+            //long to long, float, or double
+            if (classEquals(c1, Long.class) || classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }            
+        } else if (classEquals(Short.class, c2)) {
+            //byte to byte, short, int, long, float, or double
+            if (classEquals(c1, Short.class) ||  classEquals(c1, Integer.class) || classEquals(c1, Long.class) || classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }            
+        } else if (classEquals(Boolean.class, c2)) {
+            //boolean to boolean
+            if (classEquals(c1, Boolean.class)) {
+                return true;
+            }
+        } else if (classEquals(Float.class, c2)) {
+            //float to float, or double
+            if (classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }                        
+        } else if (classEquals(Character.class, c2)) {
+            //char to char, int, long, float, or double
+            if (classEquals(c1, Character.class) || classEquals(c1, Integer.class) || classEquals(c1, Long.class) || classEquals(c1, Float.class) || classEquals(c1, Double.class)) {
+                return true;
+            }            
+        }
+        return false;
+    }
+    
+    private static boolean classEquals(Class a, Class b) {
+        return a.getName().equals(b.getName());
+    }
+    
+    private static boolean isPrimitive(Class c) {
+        return c.isPrimitive() 
+        || classEquals(Integer.class, c)
+        || classEquals(Double.class, c)
+        || classEquals(Byte.class, c)
+        || classEquals(Long.class, c)
+        || classEquals(Short.class, c)
+        || classEquals(Boolean.class, c)
+        || classEquals(Float.class, c)
+        || classEquals(Character.class, c);
+    }
 
 }
 
