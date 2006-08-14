5a231c191426df30e4bf49612d9e7cd0b816d18a
jeliot <jeliot@cs.uef.fi>
2006-08-14 14:33:24 +0000

    Adaptation package added -AM

---
+++ src/jeliot/adapt/BasicInternalUM.java
@@ -0,0 +1,36 @@
+package jeliot.adapt;
+
+public class BasicInternalUM implements UMInteraction{
+
+	//Right now properties are just variables
+	// TODO: To be changed to something more OO
+	
+	// Number represents number of right questions on the topic 
+	public int assignment=0;
+
+	public void userLogon(String userName, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void userLogin(String userName, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void userLogout(String userName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void recordEvent(ModelEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double getConceptKnowledge(String concept) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
