f2df2e2293f01369edfa3a7dc246e4782f02dd7a
jeliot <jeliot@cs.uef.fi>
2007-06-06 12:30:41 +0000

    New examples for string and scanner utilities -AM

--- src/examples/BankAccount.java
+++ src/examples/BankAccount.java
@@ -16,15 +16,19 @@ public class BankAccount{
     this.accountID = read.nextLong();
     this.balance = read.nextDouble(); 
   }
-
+  //Constructor with parameters
   BankAccount(String name, long accountID, double balance){
     this.ownerName = name;
     this.accountID = accountID;
     this.balance = balance; 
   }
+  //Deposit adds an amount to the balance
   public void deposit(double amount){
      this.balance += amount;
   }
+  //Withdraw an amount to the balance, 
+  //and returns a boolean indicating
+  //the success of the operation  
   public boolean withdraw(double amount){
      boolean withdrawn = false;
      if (balance > amount){
@@ -36,8 +40,8 @@ public class BankAccount{
     }
     return withdrawn;
   }
-  }
 }
+
 
 public class MyClass {
   public static void main() {
