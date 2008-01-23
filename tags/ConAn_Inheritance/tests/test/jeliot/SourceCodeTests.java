8a0d48b6ea2c20d61faf36de721892d5686f7da2
nobody <nobody@e970d705-5e49-0410-aae4-19a007768b50>
2008-01-23 13:36:32 +0000

    This commit was manufactured by cvs2svn to create tag 'ConAn_Inheritance'.

---
+++ tags/ConAn_Inheritance/tests/test/jeliot/SourceCodeTests.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 11.1.2006
+ */
+package test.jeliot;
+
+import jeliot.util.SourceCodeUtilities;
+
+public class SourceCodeTests {
+
+    public SourceCodeTests() {
+        super();
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(SourceCodeUtilities.removeComments("/*\n*\nsdfasdf\\dfsasf\nadfsasf*/va\n/* // */in // t‰m‰ ei tuu\n//*********> Algorithm starts here\n// Define the interval [a,b] in which you expect\n// a root\n t‰m‰ ja /* // */ t‰m‰ pit‰isi /*   */ j‰‰d‰ /*\n t‰m‰ ei tuu\n // */\n"));
+    }
+
+}
