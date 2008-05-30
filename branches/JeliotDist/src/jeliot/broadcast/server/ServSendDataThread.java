9faf4d04df724bfdf852bcb6b030641e0addfb51
carlosmll <carlosmll@e970d705-5e49-0410-aae4-19a007768b50>
2008-05-30 09:31:48 +0000

    Line 72: Method public void setMCode(String str) to get MCode from TheaterMCodeInterpreter

--- branches/JeliotDist/src/jeliot/broadcast/server/ServSendDataThread.java
+++ branches/JeliotDist/src/jeliot/broadcast/server/ServSendDataThread.java
@@ -1,12 +1,8 @@
 package jeliot.broadcast.server;
-/**
- *
- * @author Carlos Menéndez
- */
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import jeliot.mcode.*;
 
 public class ServSendDataThread extends Thread{
     
@@ -17,25 +13,26 @@ public class ServSendDataThread extends Thread{
     private ServMessages message = null;
     public boolean flagExit = false;
     public boolean priorityConceded = false;
-    public TestForm window = null;
     public String text = null;
-     
+
     /* Creates a new instance of ServSendDataThread */
     public ServSendDataThread(Server server, ConnectionTable connectionTable, ServMessages message) {
-            this.connectionTable = connectionTable;
-            this.server = server;
-            this.message = message;
-            this.window = new TestForm();
+        this.inputline = new BufferedReader(new InputStreamReader(System.in));
+        this.connectionTable = connectionTable;
+        this.server = server;
+        this.message = message;
+
     }
     
     public void run(){
-        //String text = window.getMessage();
-        
-        
+
         while(true){
             //Read line from input
-
-            //text = window.getMessage();
+            /*try {
+                text = inputline.readLine();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }*/
         
             if(text != null){
                 if((text.indexOf("Exit") != -1)){
@@ -44,7 +41,7 @@ public class ServSendDataThread extends Thread{
                         break; 
                     }
                 }
-                //System.out.println("Data -->" +  text + "\n");
+
                 //Broadcasting sendToClient to every client
                 if(text.length() != 0){
                     Broadcast(text);
@@ -76,6 +73,6 @@ public class ServSendDataThread extends Thread{
         text = new String(str);
         System.out.println("Codigo " + str);
     }
-
+    
 }//End Thread
 
