590745607572c2ec8b1d2bf1725f78c60af52e5d
andresfib@gmail.com <andresfib@gmail.com@e970d705-5e49-0410-aae4-19a007768b50>
2009-07-29 06:23:36 +0000

    Created branch to develop explanations, to be implemented by Peng Wang

---
+++ branches/explanations/testFiles/consInsideCons.java
@@ -0,0 +1,27 @@
+//Class:MyClass
+
+public class Side {
+    public int length;
+    Side(){
+        length=0;
+    }
+    Side(int l){
+        length = l;
+    }
+}
+
+public class Square {
+    Side side;
+    Square(){
+      this(new Side(0));
+    }
+    Square(Side s){       
+        side= s;
+    }
+}
+
+public class MyClass {
+    public static void main() {
+        Square square = new Square();
+    }
+}
