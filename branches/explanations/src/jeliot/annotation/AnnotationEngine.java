a39ce44f7d3b7b36ffb43023824edc767e4d55fa
pwang86 <pwang86@e970d705-5e49-0410-aae4-19a007768b50>
2009-09-15 07:48:01 +0000

    small changes about content in message box when explanation.

--- branches/explanations/src/jeliot/annotation/AnnotationEngine.java
+++ branches/explanations/src/jeliot/annotation/AnnotationEngine.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
  
 public class AnnotationEngine {
 	public void explanationMCDisplay(){
-		JOptionPane.showMessageDialog(null,"This is a method call,including main method,super method and etc.","Explanation",JOptionPane.PLAIN_MESSAGE);  }
+		JOptionPane.showMessageDialog(null,"The following step is a method call,including main method,super method and etc.","Explanation",JOptionPane.PLAIN_MESSAGE);  }
 
 	public void explanationVDDisplay(){
-		JOptionPane.showMessageDialog(null,"This is a Variable declaration","Explanation",JOptionPane.PLAIN_MESSAGE);  }
+		JOptionPane.showMessageDialog(null,"The following step is a Variable declaration","Explanation",JOptionPane.PLAIN_MESSAGE);  }
 
 }
