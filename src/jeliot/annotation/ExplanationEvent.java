7bf1c50ef8490caa8473d973e5ff2748cf816249
pwang86 <pwang86@gmail.com>
2010-02-07 20:34:04 +0000

    make the position of message dialog closed to the top right of the screen.

--- src/jeliot/annotation/ExplanationEvent.java
+++ src/jeliot/annotation/ExplanationEvent.java
@@ -1,6 +1,8 @@
 package jeliot.annotation;
 
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.ResourceBundle;
 
 import javax.swing.JDialog;
@@ -14,11 +16,19 @@ import jeliot.util.ResourceBundles;
 
 public class ExplanationEvent {
 	static private ResourceBundle messageBundle = ResourceBundles.getTheaterMessageResourceBundle();
-
+	Dimension d_screen = Toolkit.getDefaultToolkit ().getScreenSize();
+	
+	//Set coordinates for message dialog to make it closed to the top-right of the screen.
+	double x1= 5*d_screen.getWidth()/8;
+	double y1= d_screen.getHeight()/22;
+	int x= new Double(x1).intValue();
+	int y= new Double(y1).intValue();
+	
 
 	public void explanationDisplay(String name,String explanation){
 		//First part:dialog part
-		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE); 
+		
         JDialog dialog = pane.createDialog (null, messageBundle.getString("dialog.message.title"));
         Container container = dialog.getContentPane( );
         
@@ -39,8 +49,13 @@ public class ExplanationEvent {
         dialog.setContentPane (mix);
         //Window to be sized to fit the preferred size 
         dialog.pack( );
+        
+        //Setlocation() method only has been put before Setvisible() method 
+        //because the size of the dialog has not been determined.
+        dialog.setLocation(x, y);
         dialog.setVisible(true);
-  
+
+
     } 
 	}
 
