349d5862d16bd9c18af6661a12ba0025117e2d44
andresfib@gmail.com <andresfib@users.noreply.github.com>
2010-05-29 12:11:11 +0000

    prettified source code prior to merge with trunk

--- src/jeliot/annotation/AnnotationEngine.java
+++ src/jeliot/annotation/AnnotationEngine.java
@@ -12,42 +12,42 @@ import java.util.ResourceBundle;
 
 
 public class AnnotationEngine {
-    static private ResourceBundle messageBundle = ResourceBundles.getAnnotationMessageResourceBundle();
-	
-    /**
+	static private ResourceBundle messageBundle = ResourceBundles.getAnnotationMessageResourceBundle();
+
+	/**
 	 * This class is used to pass the different events to the InfoPanel.
 	 */
-    ExplanationEvent explanationevent = new ExplanationEvent();
-    
-    /**
-    *This is determined whether showing the message dialog or not,show is the trigger. 
-    */
-    private UserProperties jeliotUserProperties = ResourceBundles
-    .getJeliotUserProperties();
-    private Boolean show = jeliotUserProperties.getBooleanProperty("show_annotations");
-	
-    //for explaining constructor
-    private String constructorname;
-	
-    //for explaining arguments
+	ExplanationEvent explanationevent = new ExplanationEvent();
+
+	/**
+	 *This is determined whether showing the message dialog or not,show is the trigger. 
+	 */
+	private UserProperties jeliotUserProperties = ResourceBundles
+	.getJeliotUserProperties();
+	private Boolean show = jeliotUserProperties.getBooleanProperty("show_annotations");
+
+	//for explaining constructor
+	private String constructorname;
+
+	//for explaining arguments
 	private String argumentexplanation;
 	private String argumentshow;
 	private String variable;
-	
+
 	//for explain object
 	private String object;
-	
+
 	//for explain field
 	private String objectfield;	
-	
+
 	//for explain arrow
 	private String value;
-	
+
 	//for explaining reference to the object of the class
 	private String reference;
-	
+
 	/*private boolean isInConstructor()throws Exception{
-		
+
 		return true;
 	}*/ 
 	public void setConstructorCall(String methodCall){
@@ -56,72 +56,72 @@ public class AnnotationEngine {
 	public String getConstructorCall(Value[] v){
 		//Determining if the constructor is a non-parameter one. 
 		if(v.length != 0)
-		   return constructorname + "( ):" + messageBundle.getString("message.constructor_call");
+			return constructorname + "( ):" + messageBundle.getString("message.constructor_call");
 		else
-		   return constructorname + "( ):" + messageBundle.getString("message.constructor_call")+messageBundle.getString("message.nonparamconstructor_call");
+			return constructorname + "( ):" + messageBundle.getString("message.constructor_call")+messageBundle.getString("message.nonparamconstructor_call");
 	}
-	
 
-    public void setArgument(Variable[] val,int n,String var){
-    	String arguments = messageBundle.getString("message.argument1");
-    	this.variable = var;
-    	this.argumentexplanation = messageBundle.getString("message.argument_explanation");
-    	String pointerargument = messageBundle.getString("message.pointerargument");
-    	for(int i=0;i<n;i++){
-        String type = val[i].getType();
-        //Determining if the argument is a primitive type.
-        if (MCodeUtilities.isPrimitive(type)) {
-    	    Value argumentvalue = val[i].getValue();
-    	    String value = argumentvalue.toString();
-    	    arguments = arguments + " " + value + ",";
-    	    this.argumentshow = arguments;
-        }
-        else{       	
-        	arguments += pointerargument;
-        	this.argumentexplanation += messageBundle.getString("message.pointerarg_explanation");
-        }
-    }
-    }
-    public String getArgument(){
- 
-    	   return argumentshow + " "+ messageBundle.getString("message.argument2")+ " " + variable + "().";
 
-    }
-    
-    
-    public void setObject(String name){
-    	this.object = name;
-    	
-    }
-    public String getObject(boolean a){
-    	if(a == true)
-    	   return object + " " + messageBundle.getString("message.object");
-    	else 
-    	   return object + " " + messageBundle.getString("message.objectdelete");
-    }
-    
-    
-    public void setObjectField(String name){
-    	this.objectfield = name;
-    }
-    public String getObjectField(){
-    	return messageBundle.getString("message.objectfield")+ objectfield + ".";
-    }
-    
-    public void setArrow(String v){
-    	
-    	this.value = v;
-    }
-    public String getArrow(){
-    	return  value + " " +messageBundle.getString("message.arrow");
-    }
-    
-    public void setReference(String ref){
-    	this.reference = ref;
-    }
-    public String getReference(){
-    	return reference;
-    }
+	public void setArgument(Variable[] val,int n,String var){
+		String arguments = messageBundle.getString("message.argument1");
+		this.variable = var;
+		this.argumentexplanation = messageBundle.getString("message.argument_explanation");
+		String pointerargument = messageBundle.getString("message.pointerargument");
+		for(int i=0;i<n;i++){
+			String type = val[i].getType();
+			//Determining if the argument is a primitive type.
+			if (MCodeUtilities.isPrimitive(type)) {
+				Value argumentvalue = val[i].getValue();
+				String value = argumentvalue.toString();
+				arguments = arguments + " " + value + ",";
+				this.argumentshow = arguments;
+			}
+			else{       	
+				arguments += pointerargument;
+				this.argumentexplanation += messageBundle.getString("message.pointerarg_explanation");
+			}
+		}
+	}
+	public String getArgument(){
+
+		return argumentshow + " "+ messageBundle.getString("message.argument2")+ " " + variable + "().";
+
+	}
+
+
+	public void setObject(String name){
+		this.object = name;
+
+	}
+	public String getObject(boolean a){
+		if(a == true)
+			return object + " " + messageBundle.getString("message.object");
+		else 
+			return object + " " + messageBundle.getString("message.objectdelete");
+	}
+
+
+	public void setObjectField(String name){
+		this.objectfield = name;
+	}
+	public String getObjectField(){
+		return messageBundle.getString("message.objectfield")+ objectfield + ".";
+	}
+
+	public void setArrow(String v){
+
+		this.value = v;
+	}
+	public String getArrow(){
+		return  value + " " +messageBundle.getString("message.arrow");
+	}
+
+	public void setReference(String ref){
+		this.reference = ref;
+	}
+	public String getReference(){
+		return reference;
+	}
 	/**
 	 * TabbedPane is not fit in this situation.
 	 */
@@ -148,32 +148,32 @@ public class AnnotationEngine {
 	            return null;
 	        }
 	    }*/
-    
-    //This method is used to give information of Constructor to Info Panel. 
-    public void explainConstructor(Value[] value,String name){
-    	
-    	if(show){
-    	setConstructorCall(name);
-    	String a = getConstructorCall(value);
-    	String b = messageBundle.getString("message.constructor_explanation");
-    	 
-    	explanationevent.explanationDisplay(a,b);
-    	}
-    	else 
-    		return ;
-    }
-    //This method is used to give information of arguments to Info Panel.
-    public void explainArgument(Variable[] val,Value[] args,String var){
-    	
-    	
-    	if(show){
-    		//determining whether arguments are not null 
-    	if (args != null && args.length > 0){
-    	setArgument(val,args.length,var);
-        String a = getArgument();      
-    	String b = argumentexplanation.toString();
-    	explanationevent.explanationDisplay(a,b);
-    	/*String type = val[i].getType();
+
+	//This method is used to give information of Constructor to Info Panel. 
+	public void explainConstructor(Value[] value,String name){
+
+		if(show){
+			setConstructorCall(name);
+			String a = getConstructorCall(value);
+			String b = messageBundle.getString("message.constructor_explanation");
+
+			explanationevent.explanationDisplay(a,b);
+		}
+		else 
+			return ;
+	}
+	//This method is used to give information of arguments to Info Panel.
+	public void explainArgument(Variable[] val,Value[] args,String var){
+
+
+		if(show){
+			//determining whether arguments are not null 
+			if (args != null && args.length > 0){
+				setArgument(val,args.length,var);
+				String a = getArgument();      
+				String b = argumentexplanation.toString();
+				explanationevent.explanationDisplay(a,b);
+				/*String type = val[i].getType();
     	//Determining if the argument is a primitive type.
         if (MCodeUtilities.isPrimitive(type)) {
         	setArgument(val[i],var);    	
@@ -182,138 +182,101 @@ public class AnnotationEngine {
         	explanationevent.explanationDisplay(a,b); 
         	}  
         else {
-           
+
            String c = messageBundle.getString("message.pointerargument");
            String d = messageBundle.getString("message.pointerarg_explanation");
            explanationevent.explanationDisplay(c,d);
             }
     		}*/
-    	}
-    	}
-    	else 
-    		return ;
-    }
-    //This method is to show information when object is instantiated.
-    public void explainObject(String name){
-    	if(show){
-    	setObject(name);
-    	String a = getObject(true);
-    	String b = messageBundle.getString("message.object_explanation");
-    	explanationevent.explanationDisplay(a,b);
-    	}
-    	else 
-    		return ;
-    }
-    //This method is to explain field when it is allocated. 
-    public void explainObjectField(String name){
-    	if(show){
-    	setObjectField(name);
-    	String a = getObjectField();
-    	String b = messageBundle.getString("message.objectfield_explanation");
-    	explanationevent.explanationDisplay(a,b);	
-    	}
-    	else 
-    		System.out.println();
-    }
-    /*This is for explaining arrow when it happens.When arrow appears,
+			}
+		}
+		else 
+			return ;
+	}
+	//This method is to show information when object is instantiated.
+	public void explainObject(String name){
+		if(show){
+			setObject(name);
+			String a = getObject(true);
+			String b = messageBundle.getString("message.object_explanation");
+			explanationevent.explanationDisplay(a,b);
+		}
+		else 
+			return ;
+	}
+	//This method is to explain field when it is allocated. 
+	public void explainObjectField(String name){
+		if(show){
+			setObjectField(name);
+			String a = getObjectField();
+			String b = messageBundle.getString("message.objectfield_explanation");
+			explanationevent.explanationDisplay(a,b);	
+		}
+	}
+	/*This is for explaining arrow when it happens.When arrow appears,
     that means method can be accessed by the reference to the object of the class.*/
-    public void explainArrow(String value){
-    	if(show){
-    	setArrow(value);
-    	String a = getArrow();
-    	String b = messageBundle.getString("message.arrow_explanation");
-    	explanationevent.explanationDisplay(a,b);
-    	}
-    	else 
-    		return ;
-    }
-    
-   //when return reference happens,show explanation.
-    public void explainReturnReference(){
-    	if(show){
-    		
-    		String a = messageBundle.getString("message.returnreference");
-    		String b = messageBundle.getString("message.returnref_explanation");
-    		explanationevent.explanationDisplay(a,b);
-    	}
-    	else 		
-    		return ;
-    }
-    
-    
-    
-    //when reference to the object of the class is assigned,explanation will be happened.
-    public void explainAssignReference(String type){
-    	if(show){
-    		if (MCodeUtilities.isPrimitive(type) || type.equals("null")) {
-    		}
-    		else{
-    		String a = messageBundle.getString("message.assignreference");
-    		String b = messageBundle.getString("message.assignref_explanation");
-    		if(type.charAt(0)=='['){        		
-    		       a = messageBundle.getString("message.arrayinstantiated");
-    		       b = messageBundle.getString("message.arrayinstan_explanation");}
-    		explanationevent.explanationDisplay(a,b);
-    	}
-    	}
-    	else 		
-    		return ;
-    }
-     
-    //When the object of the class or array is deleted,message dialog will be shown.
-    public void explainGarbage(String name){
-    	if(show){
-    	setObject(name);
-    	String a = getObject(false);
-    	String b = messageBundle.getString("message.objectdelete_explanation");
-    	// Symbol '[' means array.If the array is deleted,assign different arguments. 
-    	if(name.charAt(0)=='['){
-    		a = messageBundle.getString("message.arraydeleted");
-    		b = messageBundle.getString("message.arraydeleted_explanation");}
-    	explanationevent.explanationDisplay(a,b);
-    	}
-    	else 		
-    		return ;
-    }
-	/*public void explanationMCDisplay(){
-		
-		//First part:dialog part
-		JOptionPane pane = new JOptionPane (name+"(): "+messageBundle.getString("message.constructor_call"), JOptionPane.INFORMATION_MESSAGE);
-        JDialog dialog = pane.createDialog (null, messageBundle.getString("dialog.message.title"));
-        Container container = dialog.getContentPane( );
-        
-        //Second part:text area for more information
-        JTextArea text = new JTextArea (messageBundle.getString("message.constructor_explanation"), 8, 40);
-       //allow Wrapped text to fit in width
-        text.setLineWrap (true);
-        //wrapped at word boundaries
-        text.setWrapStyleWord (true);
-        text.setEditable(false);
+	public void explainArrow(String value){
+		if(show){
+			setArrow(value);
+			String a = getArrow();
+			String b = messageBundle.getString("message.arrow_explanation");
+			explanationevent.explanationDisplay(a,b);
+		}	
+	}
 
-        JScrollPane scroller =
-            new JScrollPane (text, 
-                             ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
-                             ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        //mix the first and second part 
-        InfoPanel mix = new InfoPanel (container, scroller);
-        dialog.setContentPane (mix);
-        //Window to be sized to fit the preferred size 
-        dialog.pack( );
-        dialog.setVisible(true);
-  
-    } */
+	//when return reference happens,show explanation.
+	public void explainReturnReference(){
+		if(show){
+
+			String a = messageBundle.getString("message.returnreference");
+			String b = messageBundle.getString("message.returnref_explanation");
+			explanationevent.explanationDisplay(a,b);
+		}
+	}
+
+
+
+	//when reference to the object of the class is assigned,explanation will be happened.
+	public void explainAssignReference(String type){
+		if(show){
+			if (MCodeUtilities.isPrimitive(type) || type.equals("null")) {
+			}
+			else{
+				String a = messageBundle.getString("message.assignreference");
+				String b = messageBundle.getString("message.assignref_explanation");
+				if(type.charAt(0)=='['){        		
+					a = messageBundle.getString("message.arrayinstantiated");
+					b = messageBundle.getString("message.arrayinstan_explanation");}
+				explanationevent.explanationDisplay(a,b);
+			}
+		}
+	}
+
+	//When the object of the class or array is deleted,message dialog will be shown.
+	public void explainGarbage(String name){
+		if(show){
+			setObject(name);
+			String a = getObject(false);
+			String b = messageBundle.getString("message.objectdelete_explanation");
+			// Symbol '[' means array.If the array is deleted,assign different arguments. 
+			if(name.charAt(0)=='['){
+				a = messageBundle.getString("message.arraydeleted");
+				b = messageBundle.getString("message.arraydeleted_explanation");}
+			explanationevent.explanationDisplay(a,b);
+		}
+	}
 
 }
-	/*public void explanationAADisplay(){
+/*public void explanationAADisplay(){
 		JOptionPane.showMessageDialog(null,"The following step is a array allocation","Explanation",JOptionPane.PLAIN_MESSAGE);  }
 
 	public void explanationAACDisplay(){
 		JOptionPane.showMessageDialog(null,"The following step is about that array can be accessed","Explanation",JOptionPane.PLAIN_MESSAGE);  }
-	
+
 	public void explanationRDisplay(){
 		JOptionPane.showMessageDialog(null,"The following step indicates it will return a value","Explanation",JOptionPane.PLAIN_MESSAGE);  }
 
 	public void explanationSADisplay(){
 		JOptionPane.showMessageDialog(null,"The following step is about class allocation","Explanation",JOptionPane.PLAIN_MESSAGE);  }
-	*/
+ */
 
--- src/jeliot/annotation/ExplanationEvent.java
+++ src/jeliot/annotation/ExplanationEvent.java
@@ -1,14 +1,7 @@
 package jeliot.annotation;
 
-import java.awt.AlphaComposite;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.util.ResourceBundle;
 
@@ -18,11 +11,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
-import sun.java2d.pipe.SpanShapeRenderer.Composite;
-
 import jeliot.util.ResourceBundles;
-
-import com.sun.awt.AWTUtilities;
 
 
 
@@ -38,107 +27,55 @@ public class ExplanationEvent {
 	double y1= d_screen.getHeight()/22;
 	int x= new Double(x1).intValue();
 	int y= new Double(y1).intValue();
-	
+
 	//private JButton button = new JButton(messageBundle.getString("message.button1"));
 
 	public void explanationDisplay(String name,String explanation){
 		//First part:dialog part
 		Object[] options = {messageBundle.getString("message.button")};
 		//Object[] options = {messageBundle.getString("message.button"),messageBundle.getString("message.button1")};
-		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_OPTION, null, options){
-	        /* try to make JoptionPane translucent,but the following codes 
-		     * make the whole part transparent."0.4f" doesn't work.
-			protected void paintComponent(Graphics g) {  
-     
-		        The following codes comes from http://marioyohanes.wordpress.com/
-		        Graphics2D g2 = (Graphics2D)g.create();
-		        Paint p = new GradientPaint(
-		                0, 0, new Color(0,0,0,0),
-		                getWidth(), 0, new Color(0x272B39),
-		                true
-		            );
-		        g2.setPaint(p);
-		        g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-		        g2.dispose();
-		        
-			* Using the second way,it still doesn't work.
-				if(isOpaque()){  
-			    g.setColor(getBackground());
-		        g.fillRect(0,0, getWidth(),getHeight());
-			   }
-		        
-		    * Using the third way,still doesn't work.  
-		        setOpaque(false);
-		        Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                //g2d.setColor(new Color(0,0,0,0));
-		        g2d.setBackground(new Color(0,0,0,0));
-		        java.awt.Composite old = g2d.getComposite();
-		        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
-		        g2d.fillRect(0,0, getWidth(),getHeight());
-		        g2d.setComposite(old);
-		        g2d.dispose();
-			}*/
-		}; 
+		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_OPTION, null, options); 
 
 		JDialog dialog = pane.createDialog(null,messageBundle.getString("dialog.message.title"));
 		Container content = dialog.getContentPane( );
 
 		//Second part:text area for more information
-		JTextArea text = new JTextArea (explanation, 8, 40){
+		JTextArea text = new JTextArea (explanation, 8, 40);
 
-			/*try to make JtextArea translucent,but the following codes 
-			 * make the whole part transparent."derive(0.75f)" doesn't work.
-			 
-			    protected void paintComponent(Graphics g) {  
-		        Graphics2D g2 = (Graphics2D)g.create();
-		        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		        Composite old = (Composite) g2.getComposite();
-				g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER).derive(0.75f));
-				g2.fillRect(0,0, getColumns(),getRows());
-				g2.setComposite((java.awt.Composite) old);
-				g2.dispose();
-			}*/	
-		};
-		
 		//allow Wrapped text to fit in width
 		text.setLineWrap (true);
 		//wrapped at word boundaries
 		text.setWrapStyleWord (true);
 		text.setEditable(false);
-		
+
 
 		JScrollPane scroller =
 			new JScrollPane (text, 
 					ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
 					ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
-		   /* try to make JscrollPane translucent.There are three steps:
-		    * --set scroller translucent;
-		    * --set getviewport translucent;
-		    * --set text on the scroller translucent;
-		    * However,these don't work.
+
+		/* try to make JscrollPane translucent.There are three steps:
+		 * --set scroller translucent;
+		 * --set getviewport translucent;
+		 * --set text on the scroller translucent;
+		 * However,these don't work.
 		    scroller.setOpaque(false);
             scroller.getViewport().setOpaque(false);
 		    text.setOpaque(false);*/
-		
+
 		//mix the first and second part 
 		InfoPanel mix = new InfoPanel(content,scroller);
 		dialog.setContentPane(mix);
-		
+
 		dialog.pack();
-        /* Setlocation() method only has been put before Setvisible() method 
+		/* Setlocation() method only has been put before Setvisible() method 
         because the size of the dialog has not been determined.*/
 		dialog.setLocation(x, y);
 		dialog.setVisible(true);
 
 
-		/* This is for making Window translucent.Unfortunately,I use Jdialog as the 
-		if (AWTUtilities.isTranslucencySupported(AWTUtilities.Translucency.TRANSLUCENT)) {
-			//perform translucency operations here
-		}*/
-    } 
+	} 
 
 
-	}
+}
 
