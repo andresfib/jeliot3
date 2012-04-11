811447560118b89421fe4a8e81ae629c6a596472
pwang86 <pwang86@gmail.com>
2012-04-11 15:43:36 +0000

    add explanations in message.properties integrate ExplanationEvent.java and InfoPanel.java

--- src/jeliot/annotation/ExplanationEvent.java
+++ src/jeliot/annotation/ExplanationEvent.java
@@ -1,17 +1,35 @@
 package jeliot.annotation;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
+import sun.java2d.pipe.SpanShapeRenderer.Composite;
+
 import jeliot.util.ResourceBundles;
+
+import com.sun.awt.AWTUtilities;
 
 
 
@@ -27,55 +45,158 @@ public class ExplanationEvent {
 	double y1= d_screen.getHeight()/22;
 	int x= new Double(x1).intValue();
 	int y= new Double(y1).intValue();
-
-	//private JButton button = new JButton(messageBundle.getString("message.button1"));
-
+	//use "show" to control JScrollPanel 
+	public boolean show = false;
 	public void explanationDisplay(String name,String explanation){
 		//First part:dialog part
+		
 		Object[] options = {messageBundle.getString("message.button")};
 		//Object[] options = {messageBundle.getString("message.button"),messageBundle.getString("message.button1")};
-		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_OPTION, null, options); 
+		JOptionPane pane = new JOptionPane (name, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_OPTION, null, options){
+	        /* try to make JoptionPane translucent,but the following codes 
+		     * make the whole part transparent."0.4f" doesn't work.
+			protected void paintComponent(Graphics g) {  
+     
+		        The following codes comes from http://marioyohanes.wordpress.com/
+		        Graphics2D g2 = (Graphics2D)g.create();
+		        Paint p = new GradientPaint(
+		                0, 0, new Color(0,0,0,0),
+		                getWidth(), 0, new Color(0x272B39),
+		                true
+		            );
+		        g2.setPaint(p);
+		        g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+		        g2.dispose();
+		        
+			* Using the second way,it still doesn't work.
+				if(isOpaque()){  
+			    g.setColor(getBackground());
+		        g.fillRect(0,0, getWidth(),getHeight());
+			   }
+		        
+		    * Using the third way,still doesn't work.  
+		        setOpaque(false);
+		        Graphics2D g2d = (Graphics2D) g.create();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                //g2d.setColor(new Color(0,0,0,0));
+		        g2d.setBackground(new Color(0,0,0,0));
+		        java.awt.Composite old = g2d.getComposite();
+		        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
+		        g2d.fillRect(0,0, getWidth(),getHeight());
+		        g2d.setComposite(old);
+		        g2d.dispose();
+			}*/
+		}; 
 
 		JDialog dialog = pane.createDialog(null,messageBundle.getString("dialog.message.title"));
 		Container content = dialog.getContentPane( );
 
 		//Second part:text area for more information
-		JTextArea text = new JTextArea (explanation, 8, 40);
+		JTextArea text = new JTextArea (explanation, 8, 40){
 
+			/*try to make JtextArea translucent,but the following codes 
+			 * make the whole part transparent."derive(0.75f)" doesn't work.
+			 
+			    protected void paintComponent(Graphics g) {  
+		        Graphics2D g2 = (Graphics2D)g.create();
+		        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		        Composite old = (Composite) g2.getComposite();
+				g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER).derive(0.75f));
+				g2.fillRect(0,0, getColumns(),getRows());
+				g2.setComposite((java.awt.Composite) old);
+				g2.dispose();
+			}*/	
+		};
+		
 		//allow Wrapped text to fit in width
 		text.setLineWrap (true);
 		//wrapped at word boundaries
 		text.setWrapStyleWord (true);
 		text.setEditable(false);
-
-
-		JScrollPane scroller =
+		//set scroller final because it will be used in a inner class
+		final JScrollPane scroller =
 			new JScrollPane (text, 
 					ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
 					ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-		/* try to make JscrollPane translucent.There are three steps:
-		 * --set scroller translucent;
-		 * --set getviewport translucent;
-		 * --set text on the scroller translucent;
-		 * However,these don't work.
+		
+		   /* try to make JscrollPane translucent.There are three steps:
+		    * --set scroller translucent;
+		    * --set getviewport translucent;
+		    * --set text on the scroller translucent;
+		    * However,these don't work.
 		    scroller.setOpaque(false);
             scroller.getViewport().setOpaque(false);
 		    text.setOpaque(false);*/
+		final JButton button = new JButton(messageBundle.getString("message.button1"));
+		//mix the first and second part,another way is:InfoPanel mix = new InfoPanel(content,scroller);
+		
+		final JPanel jpanel = new JPanel(){
+			////make background of Jbutton grey
+			protected void paintComponent(Graphics g){  
+				Graphics2D g2d = (Graphics2D)g.create();
+				Paint p = new GradientPaint(
+		                0, 0, new Color(0,0,0,0),
+		                getWidth()/2, 0, new Color(66,66,66),
+		                true
+		            );
+		        g2d.setPaint(p);
+		        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+		        g2d.dispose();
+			}
+		};
 
-		//mix the first and second part 
-		InfoPanel mix = new InfoPanel(content,scroller);
-		dialog.setContentPane(mix);
+		ActionListener al = new ActionListener(){
 
+			public void actionPerformed(ActionEvent e) { 
+
+				show =! show;
+				if(show)
+				{
+					scroller.setVisible(!show);
+					button.setText(messageBundle.getString("message.button2"));
+					Container ancestor = jpanel.getTopLevelAncestor( ); 
+					if ((ancestor != null) && (ancestor instanceof Window))        
+						((Window) ancestor).pack( );
+					jpanel.repaint( );
+					
+				}
+				else 
+				{    
+					scroller.setVisible(!show);
+					button.setText(messageBundle.getString("message.button1"));
+					//show the bottom part again
+					Container ancestor = jpanel.getTopLevelAncestor( ); 
+					if ((ancestor != null) && (ancestor instanceof Window))        
+						((Window) ancestor).pack( );
+					jpanel.repaint( );
+					
+					
+				}	 
+			}
+		};
+		button.addActionListener(al);
+		jpanel.setLayout(new BoxLayout (jpanel, BoxLayout.Y_AXIS));
+		//add(content) rather than add(pane) because the latter one keep the JOptionpane moved 
+		jpanel.add(content);
+		jpanel.add(button);
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		jpanel.add (scroller); 
+		
+		dialog.setContentPane(jpanel);
+		
 		dialog.pack();
-		/* Setlocation() method only has been put before Setvisible() method 
+        /* Setlocation() method only has been put before Setvisible() method 
         because the size of the dialog has not been determined.*/
 		dialog.setLocation(x, y);
 		dialog.setVisible(true);
 
 
-	} 
+		/* This is for making Window translucent.Unfortunately,I use Jdialog as the 
+		if (AWTUtilities.isTranslucencySupported(AWTUtilities.Translucency.TRANSLUCENT)) {
+			//perform translucency operations here
+		}*/
+    } 
 
 
-}
+	}
 
